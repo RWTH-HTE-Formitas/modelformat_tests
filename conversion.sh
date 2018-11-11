4c2ea6dc7bc43a836da4d9cf3cfbb08a27a406c2
Tshabalutzi <jonas.scheller@rwth-aachen.de>
2018-11-11 21:10:33 +0100

    gltf trimming

--- conversion.sh
+++ conversion.sh
@@ -1,6 +1,7 @@
-if ! [ -x "$(command -v obj2gltf)" ]; then
-  echo '>> Error: obj2gltf is not installed. download node.js and then run' >&2
+if ! [ -x "$(command -v obj2gltf)" ] || [ -x "$(command -v gltf-pipeline)" ] ]; then
+  echo '>> Install dependencies: download node.js and run' >&2
   echo '>> npm install -g obj2gltf'
+  echo '>> npm install -g gltf-pipeline'
   exit 1
 fi
 
@@ -16,7 +17,9 @@ for a in data/IFC/*.ifc ; do
     echo "converting $d"
     ./Binaries/IfcConvert data/IFC/$d.ifc  data/collada/$d.dae
     ./Binaries/COLLADA2GLTF-bin -i data/collada/$d.dae -o data/dae2gltf/$d.gltf
+    gltf-pipeline -i data/dae2gltf/$d.gltf -o data/dae2gltf/$d.opt.gltf
 
     ./Binaries/IfcConvert data/IFC/$d.ifc data/obj/$d.obj
-    obj2gltf -i data/obj/$d.obj -o data/obj2gltf/$d.gltf 
+    obj2gltf -i data/obj/$d.obj -o data/obj2gltf/$d.gltf
+    gltf-pipeline -i data/obj2gltf/$d.gltf -o data/obj2gltf/$d.opt.gltf
 done
