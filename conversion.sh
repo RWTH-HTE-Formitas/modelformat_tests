796cdffe600d51eb93693ba9d81c6756ea407433
Tshabalutzi <jonas.scheller@rwth-aachen.de>
2018-11-11 17:30:47 +0100

    added obj2gltf conversion

--- conversion.sh
+++ conversion.sh
@@ -1,4 +1,10 @@
-for format in "gltf" "collada" "obj"
+if ! [ -x "$(command -v obj2gltf)" ]; then
+  echo '>> Error: obj2gltf is not installed. download node.js and then run' >&2
+  echo '>> npm install -g obj2gltf'
+  exit 1
+fi
+
+for format in "obj2gltf" "dae2gltf" "collada" "obj"
   do
     if [ ! -d "data/$format" ]; then
       mkdir -p "data/$format";
@@ -8,7 +14,9 @@ for format in "gltf" "collada" "obj"
 for a in data/IFC/*.ifc ; do
     d=$(basename $a .ifc)
     echo "converting $d"
-    ./Binaries/IfcConvert data/IFC/$d.ifc data/obj/$d.obj
     ./Binaries/IfcConvert data/IFC/$d.ifc  data/collada/$d.dae
-    ./Binaries/COLLADA2GLTF-bin -i data/collada/$d.dae -o data/gltf/$d.gltf
+    ./Binaries/COLLADA2GLTF-bin -i data/collada/$d.dae -o data/dae2gltf/$d.gltf
+
+    ./Binaries/IfcConvert data/IFC/$d.ifc data/obj/$d.obj
+    obj2gltf -i data/obj/$d.obj -o data/obj2gltf/$d.gltf 
 done
